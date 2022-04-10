f42af4e66d1a80c1c405741423aa4d6cd5bbd4b8
Carlos Marinho <carlos.marinho20@outlook.com>
2022-04-09 22:34:34 -0300

    feat(structure) - refatora componente _Product_
    Refatora componente product, mudando alguns estilos e removendo
    componentes que não serão utilizados.
    
    Adiciona _model_ do componente.

--- src/components/Product/index.tsx
+++ src/components/Product/index.tsx
@@ -1,28 +1,22 @@
+import Button from "../Button";
 import Incrementor from "../Incrementor";
-import { Wrapper, Info, Column, Text, WrapperIncrementor } from "./Product.styles";
+import { ProductProps } from "./Product.model";
+import { Container, Image } from "./Product.styles";
 
-export type ProductProps = {
-  id: string;
-  name: string;
-  price: number;
-  picture: string;
+const Product = ({ id, name, price, picture }: ProductProps) => {
+  const handleQuantityChange = (type: "increment" | "decrement") => {
+    //TODO
+  };
+
+  return (
+    <Container>
+      <Image src={picture} alt={`Imagem de referência ${name}`} />
+      <span>{name}</span>
+      <span>{price}</span>
+      <Incrementor quantity={1} />
+      <Button>Adicionar ao carrinho</Button>
+    </Container>
+  );
 };
-
-const Product = ({ id, name, price, picture }: ProductProps) => (
-  <Wrapper>
-    <img src={picture} alt={`Imagem de referência ${name}`} />
-
-    <Info>
-      <Column>
-        <Text>{name}</Text>
-        <Text>{price}</Text>
-      </Column>
-
-      <WrapperIncrementor>
-        <Incrementor quantity={1} />
-      </WrapperIncrementor>
-    </Info>
-  </Wrapper>
-);
 
 export default Product;
